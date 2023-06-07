b566bfd78e7fc0f1fbb64c4ad50bccb8422d971c
jean-baptiste faria <fariajeanbap@gmail.com>
2023-06-07 23:25:02 +0200

    ajout d'un seed pour simul les produit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+require 'faker'
+# Create 20 products
+# db/seeds.rb
+
+# Create 20 products
+20.times do
+  product = Product.new(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.sentence,
+    price: Faker::Commerce.price(range: 0..100.0)
+  )
+  product.save
+end
+
