29b76f9d64ba043657103101cba989c384555349
jean-baptiste faria <fariajeanbap@gmail.com>
2023-06-08 17:45:05 +0200

    ajout stripe

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,7 @@ class ProductsController < ApplicationController
 
   # GET /products/1 or /products/1.json
   def show
+    
     @carts = Cart.find(params[:cart_id])
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -24,9 +24,9 @@ Rails.application.routes.draw do
   end
 
   # Route pour afficher le panier
-  get 'cart', to: 'cart#show', as: 'view_cart'
+  get 'cart', to: 'carts#show', as: 'view_cart'
   get '/carts/:id', to: 'cart#show', as: 'cart'
-  
+
   # Route pour ajouter un produit au panier
   post 'cart/add'
 
